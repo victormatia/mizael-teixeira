764c50e57e36c977f3eb8703ad34caeaab21e3c6
victormatia <fvictor1705@gmail.com>
2025-02-10 16:39:17 -0300

    feat: create urls file

---
+++ src/constants/urls.ts
@@ -0,0 +1,5 @@
+export const BASE_URL = 'http://localhost:3000/api/';
+
+export const GENDERS_URL = BASE_URL + 'genders';
+export const MUSICS_URL = BASE_URL + 'musics';
+export const MUSIC_BY_GENDER = BASE_URL + 'musics/gender/';
