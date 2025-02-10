dadc89a7332ef9e84303ffa64013c4107c2f2de6
victormatia <fvictor1705@gmail.com>
2025-02-10 16:55:23 -0300

    refactor: get env host on url file

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,4 +1,6 @@
-export const BASE_URL = 'http://localhost:3000/api/';
+const HOST = process.env.NEXT_PUBLIC_HOST || 'http://localhost:3000';
+
+export const BASE_URL = HOST + '/api/';
 
 export const GENDERS_URL = BASE_URL + 'genders';
 export const MUSICS_URL = BASE_URL + 'musics';
