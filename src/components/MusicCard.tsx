1a904398f5e11717abdf7b583e633a1fd51e11e7
victormatia <fvictor1705@gmail.com>
2025-02-10 16:40:33 -0300

    feat: finish MusicCard component

--- src/components/MusicCard.tsx
+++ src/components/MusicCard.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Card, CardContent, CardDescription, CardTitle } from './ui/card';
 import { FileSpreadsheet } from 'lucide-react';
+import { Music } from '@prisma/client';
+import { usePathname } from 'next/navigation';
 
 type TProps = {
-  music: {
-    id: string | number;
-  };
+  music: Music;
 };
 
 export default function MusicCard({ music }: TProps) {
+  const path = usePathname();
+
   return (
     <Card className="max-w-[225px]">
       <CardContent className="flex flex-col gap-4 p-4">
         <div className="flex h-[180px] w-[190px] items-center justify-center rounded-sm bg-zinc-300">
           <FileSpreadsheet color="gray" />
         </div>
-        <CardTitle>
-          <p>Música {music.id}</p>
-        </CardTitle>
+        <CardTitle>{music.name}</CardTitle>
+        <CardDescription className={path === '/' ? 'w-32 truncate' : ''}>{music.autor}</CardDescription>
         <CardDescription>R$ 12,00</CardDescription>
       </CardContent>
     </Card>
