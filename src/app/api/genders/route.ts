b4d97f6f387182911d881f9931892081b311887f
victormatia <fvictor1705@gmail.com>
2025-02-10 16:42:54 -0300

    feat: creates api/genders api route

---
+++ src/app/api/genders/route.ts
@@ -0,0 +1,21 @@
+import prisma from '@/lib/prisma';
+
+async function main() {
+  const products = await prisma.gender.findMany();
+  return products;
+}
+
+export const dynamic = 'force-dynamic';
+
+export async function GET() {
+  return main()
+    .then(async (data) => {
+      await prisma.$disconnect();
+      return Response.json(data);
+    })
+    .catch(async (e) => {
+      console.error(e);
+      await prisma.$disconnect();
+      return Response.json({ message: e.message });
+    });
+}
